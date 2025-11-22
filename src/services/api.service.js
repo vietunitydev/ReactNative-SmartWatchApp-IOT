278ed2d58109bfffbe4b64a6f8f12da5f339cfb8
viet <vietunitydev2222@gmail.com>
2025-11-22 21:38:43 +0700

    - fix error api

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {Dayjs as Date} from "dayjs";
 
 // Cấu hình base URL
 const BASE_URL = 'http://10.0.2.2:8080/api';
